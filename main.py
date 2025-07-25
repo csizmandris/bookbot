2d867f33ed7812035dfad11d0ea739228b5c9a56
csizmandris <andrascsizma@gmail.com>
2025-07-25 23:16:13 +0200

    Bookbot v1.0

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
+import sys
+from stats import (
+    get_book_text,
+    count_words,
+    count_characters,
+    sort_list,
+)
+
+sys.argv
+
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = count_words(text)
-    characters = count_characters(text)
-    sorted_list = sort_list(characters)
-    #printable_list = function? str.isalpha()
-
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for items in sorted_list:
-        print(f"{items["character"]}: {items["num"]}")
-    print("============= END ===============")
-
-
-from stats import get_book_text
-
-from stats import count_words
-
-from stats import count_characters
-
-from stats import sort_list
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = count_words(text)
+        characters = count_characters(text)
+        sorted_list = sort_list(characters)
+        
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for items in sorted_list:
+            print(f"{items["character"]}: {items["num"]}")
+        print("============= END ===============")
 
 main()
